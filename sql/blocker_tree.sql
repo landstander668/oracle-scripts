660b0fe6f039d924e2a14bff4a20869cf2ff6e64
Adric Norris <landstander668@gmail.com>
2018-03-20 12:54:06 -0500

    Add parallel server ID to blocker tree output
    Add a PQ column, which indicates the parallel query server (P001, for
    example) when applicable. This makes it easier to determine when one
    thread of a parallel query is blocking another, which is part of normal
    parallel operation... this is very likely to be the case when both
    USERNAME and SQL_ID match within a given branch. Most people wouldn't
    consider this a *true* blocking lock, since it isn't affecting other
    sessions.
    
    Example output (with column lengths shortened):
    
    SQL> @blocker_tree
    
    Lock Tree                 USERNAME        SQL_ID        STATUS   PQ
    ------------------------- --------------- ------------- -------- ----
    1.2448.49655              SYSTEM          1crqrd757d0mf ACTIVE   P002
      1.4313.15755            SYSTEM          1crqrd757d0mf ACTIVE

--- sql/blocker_tree.sql
+++ sql/blocker_tree.sql
@@ -1,7 +1,8 @@
 -- Display a RAC-aware hierarchical tree of blocking/waiting sessions
 --
-column lock_tree heading "Lock Tree" format a60
+column lock_tree heading "Lock Tree" format a55
 column username format a40
+column pq format a4
 with sess as (
    select /*+ MATERIALIZE */ s.inst_id, s.sid, s.serial#, s.type, s.username, s.sql_id, s.status, s.program,
           s.blocking_instance, s.blocking_session
@@ -11,11 +12,17 @@ lk as (
    select s1.blocking_instance || '.' || s1.blocking_session || '.' || s2.serial# blocker, s2.status blocking_status,
           s2.sql_id blocking_sql_id, s1.inst_id || '.' || s1.sid || '.' || s1.serial# waiter, s1.status,
           s1.username waiter_username,
-          case when s2.type = 'BACKGROUND' then 'System: ' || regexp_replace( s2.program, '^[^(]*\(([[:alnum:]]+)\)[^)]*$', '\1' )
+          case when s2.type = 'BACKGROUND' then 'System: ' || substr( s2.program, -5, 4 )
                when s2.username is NULL    then 'Unknown: possibly job/scheduler related'
                else s2.username
           end blocker_username,
-          s1.sql_id
+          s1.sql_id,
+          case when s1.type = 'USER' and regexp_like( s1.program, '^oracle@[^(]+\(P[[:digit:]]{3}\)$' ) then substr( s1.program, -5, 4 )
+               else NULL
+          end waiter_pq,
+          case when s2.type = 'USER' and regexp_like( s2.program, '^oracle@[^(]+\(P[[:digit:]]{3}\)$' ) then substr( s2.program, -5, 4 )
+               else NULL
+          end blocker_pq
       from sess s1
          join sess s2 on ( s2.inst_id = s1.blocking_instance and
                            s2.sid     = s1.blocking_session
@@ -23,13 +30,14 @@ lk as (
       where s1.blocking_instance is not null
         and s1.blocking_session  is not null
 )
-select lpad( '  ', 2*(level-1) ) || t.waiter lock_tree, t.username, t.sql_id, t.status
-   from ( select l1.blocker, l1.waiter, l1.waiter_username username, l1.sql_id, l1.status
+select lpad( '  ', 2*(level-1) ) || t.waiter lock_tree, t.username, t.sql_id, t.status, t.pq
+   from ( select l1.blocker, l1.waiter, l1.waiter_username username, l1.sql_id, l1.status, l1.waiter_pq pq
              from lk l1
           union all
-          select distinct NULL, l2.blocker, l2.blocker_username, l2.blocking_sql_id, l2.blocking_status
+          select distinct NULL, l2.blocker, l2.blocker_username, l2.blocking_sql_id, l2.blocking_status, l2.blocker_pq
              from lk l2
              where l2.blocker not in ( select l3.waiter from lk l3 )
         ) t
    connect by prior t.waiter = t.blocker
       start with t.blocker is NULL;
+
