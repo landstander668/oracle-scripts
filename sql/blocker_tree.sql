70336d08ecf23c890e1e63a637176d7fbb12632c
Adric Norris <landstander668@gmail.com>
2018-06-19 13:22:05 -0500

    Add SQL Child Number to blocker tree output
    Add SQL child number to the output, next to the SQL ID. This is useful
    if you subsequently decide to use DBMS_XPLAN.DISPLAY_CURSOR (or
    cursor_xplan.sql) to display the execution plan for one of the statements.
    
       Example output (with column lengths shortened):
    
       SQL> @blocker_tree
    
       Lock Tree                 USERNAME        SQL_ID        CHILD STATUS   PQ
       ------------------------- --------------- ------------- ----- -------- ----
       1.2448.49655              SYSTEM          1crqrd757d0mf     9 ACTIVE   P002
         1.4313.15755            SYSTEM          1crqrd757d0mf    10 ACTIVE

--- sql/blocker_tree.sql
+++ sql/blocker_tree.sql
@@ -1,22 +1,23 @@
 -- Display a RAC-aware hierarchical tree of blocking/waiting sessions
 --
 column lock_tree heading "Lock Tree" format a55
-column username format a40
-column pq format a4
+column username                      format a40
+column child                         format 9999
+column pq                            format a4
 with sess as (
-   select /*+ MATERIALIZE */ s.inst_id, s.sid, s.serial#, s.type, s.username, s.sql_id, s.status, s.program,
+   select /*+ MATERIALIZE */ s.inst_id, s.sid, s.serial#, s.type, s.username, s.sql_id, s.sql_child_number, s.status, s.program,
           s.blocking_instance, s.blocking_session
       from gv$session s
 ),
 lk as (
    select s1.blocking_instance || '.' || s1.blocking_session || '.' || s2.serial# blocker, s2.status blocking_status,
-          s2.sql_id blocking_sql_id, s1.inst_id || '.' || s1.sid || '.' || s1.serial# waiter, s1.status,
+          s2.sql_id blocking_sql_id, s2.sql_child_number blocking_child_number, s1.inst_id || '.' || s1.sid || '.' || s1.serial# waiter, s1.status,
           s1.username waiter_username,
           case when s2.type = 'BACKGROUND' then 'System: ' || substr( s2.program, -5, 4 )
                when s2.username is NULL    then 'Unknown: possibly job/scheduler related'
                else s2.username
           end blocker_username,
-          s1.sql_id,
+          s1.sql_id, s1.sql_child_number,
           case when s1.type = 'USER' and regexp_like( s1.program, '^oracle@[^(]+\(P[[:digit:]]{3}\)$' ) then substr( s1.program, -5, 4 )
                else NULL
           end waiter_pq,
@@ -30,11 +31,11 @@ lk as (
       where s1.blocking_instance is not null
         and s1.blocking_session  is not null
 )
-select lpad( '  ', 2*(level-1) ) || t.waiter lock_tree, t.username, t.sql_id, t.status, t.pq
-   from ( select l1.blocker, l1.waiter, l1.waiter_username username, l1.sql_id, l1.status, l1.waiter_pq pq
+select lpad( '  ', 2*(level-1) ) || t.waiter lock_tree, t.username, t.sql_id, t.child, t.status, t.pq
+   from ( select l1.blocker, l1.waiter, l1.waiter_username username, l1.sql_id, l1.sql_child_number child, l1.status, l1.waiter_pq pq
              from lk l1
           union all
-          select distinct NULL, l2.blocker, l2.blocker_username, l2.blocking_sql_id, l2.blocking_status, l2.blocker_pq
+          select distinct NULL, l2.blocker, l2.blocker_username, l2.blocking_sql_id, l2.blocking_child_number, l2.blocking_status, l2.blocker_pq
              from lk l2
              where l2.blocker not in ( select l3.waiter from lk l3 )
         ) t
